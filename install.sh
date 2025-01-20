bff60c999c6151d14fe3a2fcaa8e691882ff73b0
Upokharel56 <155223739+Upokharel56@users.noreply.github.com>
2025-01-20 14:06:31 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,41 +1,44 @@
-Download_Url="https://raw.githubusercontent.com/Upokharel56/android-doctor/refs/heads/main/android-doctor_0.1.0_amd64.deb"
+# Download_Url="https://raw.githubusercontent.com/Upokharel56/android-doctor/main/android-doctor_0.1.0_amd64.deb"
 
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]; then
-    echo "This script must be run as root. Trying to elevate privileges..."
-    sudo "$0" "$@"
-    exit $?
-fi
+# # Check if the script is run as root
+# if [ "$EUID" -ne 0 ]; then
+#     echo "This script must be run as root. Trying to elevate privileges..."
+#     sudo "$0" "$@"
+#     exit $?
+# fi
 
-# Create a temporary directory
-TEMP_DIR="/usr/local/temp/utsav/temp/adoc/deb/"
-mkdir -p "$TEMP_DIR"
-cd "$TEMP_DIR"
+# # Create a temporary directory
+# TEMP_DIR="/usr/local/temp/utsav/temp/adoc/deb/"
+# mkdir -p "$TEMP_DIR"
+# cd "$TEMP_DIR"
 
-echo "Installing Android Doctor..."
-# Download the .deb file to the temporary directory
-curl -L -O https://raw.githubusercontent.com/Upokharel56/android-doctor/refs/heads/main/android-doctor_0.1.0_amd64.deb
+# echo "Installing Android Doctor..."
+# # Download the .deb file to the temporary directory
+# curl -L -O "$Download_Url"
 
-echo "\n\n\n"
-ls $TEMP_DIR
+# echo "\n\n\n"
+# ls $TEMP_DIR
 
-echo "\n\n\n"
-# Install the .deb package
+# echo "\n\n\n"
+# # Install the .deb package
+# sudo dpkg -i "$TEMP_DIR/$(basename $Download_Url)"
+
+# # Check if the installation was successful
+# if [ $? -ne 0 ]; then
+#     echo "Error: Failed to install the package."
+#     exit 1
+# fi
+
+# echo "Android Doctor has been installed successfully."
+
+# # Clean up the temporary directory
+# rm -rf "/usr/local/temp/utsav/"
+
+# # Get the original user's home directory
+# USER_HOME=$(eval echo ~$SUDO_USER)
+
 sudo dpkg -i android-doctor_0.1.0_amd64.deb
 
-# Check if the installation was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to install the package."
-    exit 1
-fi
-
-echo "Android Doctor has been installed successfully."
-
-# Clean up the temporary directory
-rm -rf "/usr/local/temp/utsav/"
-
-# Get the original user's home directory
-USER_HOME=$(eval echo ~$SUDO_USER)
 
 # Add export to bashrc and zshrc
 cat <<EOL >> "$USER_HOME/.bashrc"
